800e01c6dade2c5004cb793718979da7a359df62
banba takumi <96656737+banbatakumi@users.noreply.github.com>
2025-06-10 20:03:50 +0900

    mymathの追加&命名変更

--- ProgramV1/lib/DigitalInOut/DigitalInOut.h
+++ ProgramV1/lib/DigitalInOut/DigitalInOut.h
@@ -14,30 +14,25 @@ typedef struct {
 } DigitalIn;
 
 // DigitalOutの初期化
-static inline void DigitalOut_Init(DigitalOut *obj, GPIO_TypeDef *port, uint16_t pin) {
+static inline void DigitalOutInit(DigitalOut *obj, GPIO_TypeDef *port, uint16_t pin) {
       obj->port = port;
       obj->pin = pin;
       HAL_GPIO_WritePin(port, pin, GPIO_PIN_RESET);
 }
 
 // DigitalOutに値を書き込む
-static inline void DigitalOut_Write(DigitalOut *obj, int value) {
+static inline void DigitalWrite(DigitalOut *obj, int value) {
       HAL_GPIO_WritePin(obj->port, obj->pin, value ? GPIO_PIN_SET : GPIO_PIN_RESET);
 }
 
-// DigitalOutの状態を読む
-static inline int DigitalOut_Read(DigitalOut *obj) {
-      return HAL_GPIO_ReadPin(obj->port, obj->pin) == GPIO_PIN_SET;
-}
-
 // DigitalInの初期化
-static inline void DigitalIn_Init(DigitalIn *obj, GPIO_TypeDef *port, uint16_t pin) {
+static inline void DigitalInInit(DigitalIn *obj, GPIO_TypeDef *port, uint16_t pin) {
       obj->port = port;
       obj->pin = pin;
 }
 
 // DigitalInの状態を読む
-static inline int DigitalIn_Read(DigitalIn *obj) {
+static inline int DigitalRead(DigitalIn *obj) {
       return HAL_GPIO_ReadPin(obj->port, obj->pin) == GPIO_PIN_SET;
 }
 
--- ProgramV1/src/app/app.h
+++ ProgramV1/src/app/app.h
@@ -1,7 +1,7 @@
 #ifndef APP_H_
 #define APP_H_
 
-#include "DigitalInOut.h"
+#include "digitalinout.h"
 // #include "main.h"
 
 void setup();
