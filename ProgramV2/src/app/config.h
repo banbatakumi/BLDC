9df600eab51c894b917091aad6592598a4607e7f
banba takumi <96656737+banbatakumi@users.noreply.github.com>
2026-05-08 01:34:17 +0900

    ADCサンプリング時間を19サイクルおよび181サイクルに変更し、設定を最適化。温度制限を70°Cに更新。

--- ProgramV2/src/app/config.h
+++ ProgramV2/src/app/config.h
@@ -1,7 +1,7 @@
 #ifndef CONFIG_H_
 #define CONFIG_H_
 
-#define TEMP_LIMIT 60  // 温度制限 [°C]
+#define TEMP_LIMIT 70  // 温度制限 [°C]
 
 #define SUPPLY_VOLTAGE_MIN_LIMIT 7
 #define SUPPLY_VOLTAGE_MAX_LIMIT 20
